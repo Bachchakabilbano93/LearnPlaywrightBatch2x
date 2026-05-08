d11c3e85ab6a25e0d5ad6f4a3cecceb1ff5c398f
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2026-05-08 19:57:43 +0530

    Add JavaScript concepts and identifier literals learning materials

---
+++ chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,15 @@
+var $ = 10;
+var _a = 12;
+var p = 10;
+
+var abc123 = 34;
+
+// var 123 = 123; // Invalid identifier, cannot start with a number
+
+var Name = "Abir";
+var name = "Raj";
+// var abir bhattacharya = "hello"; // Invalid identifier, cannot contain spaces
+var abir_bhattacharya = "hello";
+var abir$bhattacharya = "hello";
+var abiru1232 = "hello";
+
