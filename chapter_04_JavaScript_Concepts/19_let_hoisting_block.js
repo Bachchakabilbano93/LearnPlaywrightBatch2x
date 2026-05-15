bcb5f3928b56afb1ea50633c2f489cfadb0c984f
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2026-05-15 09:31:06 +0530

    Add Chapter 04 let/const hoisting examples and Chapter 05 Literal types learning materials

---
+++ chapter_04_JavaScript_Concepts/19_let_hoisting_block.js
@@ -0,0 +1,11 @@
+// let is Block-Scoped
+
+let x = "global";
+if (true) {
+    // TDZ for block-scoped "x" starts here
+    // console.log(x); // ReferenceError (NOT "global"!)
+    let x = "block";
+    console.log(x);
+}
+
+console.log(x);
---
+++ chapter_05_Literal/24_null.js
@@ -0,0 +1,8 @@
+let no_audi_abir_has = null;
+let abir_has_two_hearts;
+
+let age = 0;
+
+let audi = "";
+console.log(typeof audi);
+console.log(typeof age);
