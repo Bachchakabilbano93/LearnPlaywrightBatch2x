b7cbd6b9743eeb6bf27453ab7ca3661326b7a008
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2026-05-22 14:36:30 +0530

    Add Chapter 10 loop learning materials (for, while, do-while, interview questions)

---
+++ chapter_10_Loops/73_For_Loop2.js
@@ -0,0 +1,14 @@
+// for (let abir = 0; abir < 10; abir++) {
+//     console.log(abir);
+// }
+// // 0 to 9, Times -> 10
+
+// var, let, const
+
+// for (let abir = 0; abir < 10; abir++) { // 0 to 9, Times -> 10
+//     console.log(abir);
+// }
+
+for (let _1 = 0; _1 <= 10; _1++) { // 0 to 10, Times -> 11
+    console.log(_1);
+}
---
+++ chapter_10_Loops/74_IQ.js
@@ -0,0 +1,19 @@
+// // for (let abir = 0; abir > 1; abir++) {
+// //     console.log(abir);
+// // }
+
+// // for (let abir = 0; ; abir++) {
+// //     console.log(abir);
+// // }
+
+// for (let abir = 0; abir < 18; abir++) {
+//     if (abir > 15) {
+//         console.log("Gift from dad, smartwatch this year")
+//     } else {
+//         console.log("No Gift, only good food")
+//     }
+// }
+
+for (let i = 0; i < 1; i++) {
+    console.log(i);
+}
